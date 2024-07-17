327e2dd439e87badf3fd33f4f7e3a63efe2f96b6
PedroIFC <ps404412@gmail.com>
2024-07-17 11:39:58 -0300

    add Card.vue and CardFull.vue

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,36 @@
 <script setup>
-import { ref } from "vue";
-import ExpandBox from "./components/ExpandBox.vue";
+import { ref } from 'vue'
+import ExpandBox from './components/ExpandBox.vue'
+import Card from './components/Card.vue'
+import CardFull from './components/CardFull.vue'
 
 const caixas = [
-  {id: 1, title: 'Caixa 1'},
-  {id: 2, title: 'Caixa 2', qtde: 20},
-  {id:3, title: 'Caixa 3', content:'Conteúdo secreto da caixa 3'},
-  {id:4, title: 'Caixa 4', content: 'Conteúdo da caixa', qtde: 5}
+  { id: 1, title: 'Caixa 1' },
+  { id: 2, title: 'Caixa 2', qtde: 20 },
+  { id: 3, title: 'Caixa 3', content: 'Conteúdo secreto da caixa 3' },
+  { id: 4, title: 'Caixa 4', content: 'Conteúdo da caixa', qtde: 5 }
 ]
-
-
-const mostrarSenha = ref(false)
-
-function trocarSenha() {
-  if (mudarSenha.value == "password"){
-    mudarSenha.value = "text"
-  }
-  else{
-    mudarSenha.value = "password"
-  }
-
-} 
 </script>
 
-
 <template>
-
-  <label>
-    <button @click="mostrarSenha = !mostrarSenha">Visualizar senha</button>
-    <input :type="mostrarSenha ? 'text' : 'password' ">
-  </label>
-
+<h1>Slots</h1>
+<CardFull> Função:Professor 
+  <template #header>
+    <h1>Eduardo da Silva</h1>
+    </template>
+    <template #footer>
+      link das redes sociais
+    </template>
+</CardFull>
+<CardFull> Função: Programador
+  <template #header>
+    <h1>Zé da Silva</h1>
+  </template>
+</CardFull>
+<Card>Isabelle</Card>
+<Card></Card>
+<hr>
   <expand-box v-for="caixa in caixas" :key="caixa.id" v-bind="caixa" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/CardFull.vue
@@ -0,0 +1,45 @@
+<script setup></script>
+
+<template>
+  <div class="card">
+    <div class="header">
+      <slot name="header"></slot>
+    </div>
+    <slot>Corpo Padrão do card</slot>
+    <div v-if="$slots.footer">
+        <p>Mais informações abaixo...</p>
+    </div>
+
+    <div v-if="$slots.footer" class="footer">
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  height: 200px;
+  width: 400px;
+  box-shadow: 2px 2px 3px 3px gray;
+  border-radius: 20px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.header {
+  background-color: greenyellow;
+  height: 60px;
+  width: 100%;
+  border-radius: 20px;
+}
+
+.footer {
+    background-color: greenyellow;
+    height: 60px;
+    width: 100%;
+    border-radius: 20px;
+  }
+
+</style>
